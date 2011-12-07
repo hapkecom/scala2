0e87f1b4bfe0bd5ec46a1ca7615912b2ecdd6904
Christian Hapke <info@hapke.com>
2011-12-07 13:43:31 +0100

    JSF finance project ongoing: fixed p:treeTable problem with JSF comments

--- src/main/scala/c/h/finance/PlanController.scala
+++ src/main/scala/c/h/finance/PlanController.scala
@@ -31,21 +31,27 @@ class PlanController extends Serializable {
   
   
   def getRoot(): TreeNode = {
-    root;
+    root
   }
   
   def createRoot: TreeNode = {
     root = new DefaultTreeNode("root", null) 
     
-    val einnahmen1000 = new DefaultTreeNode(new Account(1000, "Account E"), root)
+    val einnahmen1000 = new DefaultTreeNode(new Account(1000, "Account E*"), root)
     val einnahmen1010 = new DefaultTreeNode(new Account(1010, "Account EA"), einnahmen1000)
     val einnahmen1011 = new DefaultTreeNode(new Account(1011, "Account EAA"), einnahmen1010)
     val einnahmen1020 = new DefaultTreeNode(new Account(1020, "Account EB"), einnahmen1000)
 
-    val ausgaben2000 = new DefaultTreeNode(new Account(2000, "Account A"), root)
-    val ausgaben2010 = new DefaultTreeNode(new Account(2000, "Account AA"), ausgaben2000)
-    val ausgaben2020 = new DefaultTreeNode(new Account(2000, "Account AB"), ausgaben2010)
+    val ausgaben2000 = new DefaultTreeNode(new Account(2000, "Account A+"), root)
+    val ausgaben2010 = new DefaultTreeNode(new Account(2010, "Account AA"), ausgaben2000)
+    val ausgaben2020 = new DefaultTreeNode(new Account(2020, "Account AB"), ausgaben2010)
 
+    val gewinn = new DefaultTreeNode(new Account(0, "Gewinn(+)/Verlust(-)"), root)
+    new DefaultTreeNode(new Account(10, "Gewinn(+)/Verlust(-)"), gewinn)
+    
+    println("root.childs="+root.getChildren())
+    println("gewinn.childs="+gewinn.getChildren())
+    
     root
   }
   
